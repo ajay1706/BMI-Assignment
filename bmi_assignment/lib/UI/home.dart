3b51914f0867294d9175ef3b1bfa679e41339332
ajay1706 <ajay97456@gmail.com>
2019-08-31 12:52:16 +0530

    adding up the comments for the assignment

---
+++ bmi_assignment/lib/UI/home.dart
@@ -0,0 +1,90 @@
+ //Okayyy, Let's finish the functioning of the application here in the home.dart file.
+
+ /* The important question you get when you design the application!!
+
+                        whether to make a Stateful class or Stateless class ?
+                                     Umm, What you say ??
+
+                                  Kudos, If you know what to use,You can go ahead or If you can't say , no worries! Just have wild guess and
+                                  see below .
+                                  We are here to help you out..
+
+                                  You should be using a Stateful widget here for the functioning as the screen contains button
+                                   which onPressed should show you the value of the BMI index
+
+  */
+
+
+ /* Screen contains                .................. appBar.................. ,
+                                   ..................Body.....................
+                                   Your body contains Image  and 3 inputs & a
+                                   Raised Button....
+
+
+                                   Make a ListView
+
+                                   Wrap all this in a container, the add a column.
+                                   While placing the image make that placed into separate container
+                                    and use container for other 3 inputs and button
+
+
+                                    use column again to show up the other 3 inputs and button
+
+
+                                    /* The Inputs code consists of
+                                    TextField widget which has the properties of
+                                     - controller (Make a TextEditingController Object inside the HomeState
+                                     - decoration
+                                     - labeltext
+                                     -hintText
+                                     - icon
+                                     -KeyboardType
+
+                                     * Then add the raised button here...
+                                     At the bottom add the Two Text() widgets to show up the output values.import '
+
+
+
+                                    $$$$$$$$$$$$$$$$$$$$$$  Kudos !!!! You are done with UI part Successfully $$$$$$$$$$$$$$$$$$
+
+  */
+                                     ........................... Lets Shift to logic part .........................
+  */
+
+
+
+ /* BMI Categories
+
+
+                                      Your Height: 	(feet)	(inches)
+
+
+                                       Your Weight:  	 (pounds)
+
+
+                                        Your BMI:
+                                         BMI Categories:
+                                         Underweight = <18.5
+                                         Normal weight = 18.5–24.9
+                                         Overweight = 25–29.9
+                                         Obesity = BMI of 30 or greater
+                                           formula for BMI is
+
+
+                                     ##############      BMI = kg/m2    #############
+
+                                             ***   The imperial BMI formula = Weight (LBS) x 703 ÷ Height (Inches²)
+                                             ***   The metric BMI formula = Weight (KG) ÷ Height (Metres²)
+
+
+
+
+
+
+
+
+
+  */
+
+
+
---
+++ bmi_assignment/lib/main.dart
@@ -0,0 +1,6 @@
+// Lets Make an BMI Application which calculates on the basis of age height & weight.
+
+/* write your main method here and inside the Material App call the Home() widget
+
+      which you are going to call which is in the /UI/home.dart file
+ */
